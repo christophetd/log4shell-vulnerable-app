b3b14a3e5c34c0e4907fba6366d743aa8d87dc70
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2021-12-10 14:06:19 +0100

    Remove unused files and rename packages

--- src/main/java/fr/christophetd/log4shell/vulnerableapp/VulnerableAppApplication.java
+++ src/main/java/fr/christophetd/log4shell/vulnerableapp/VulnerableAppApplication.java
@@ -1,4 +1,4 @@
-package com.datadoghq.log4jexample.vulnerableapp;
+package fr.christophetd.log4shell.vulnerableapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
